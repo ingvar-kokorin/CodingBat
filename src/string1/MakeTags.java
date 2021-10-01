64c8dcc06bf48383a01f66b136a82706dec3d793
Ingvar Kokorin <ingvar.kokorin@gmail.com>
2021-10-01 20:59:32 +0300

    commit#1

--- src/string1/MakeTags.java
+++ src/string1/MakeTags.java
@@ -4,7 +4,7 @@ package string1;
  * The web is built with HTML strings like "<i>Yay</i>" which draws Yay as italic text. In this example, the "i"
  * tag makes <i> and </i> which surround the word "Yay". Given tag and word strings, create the HTML string with
  * tags around the word, e.g. "<i>Yay</i>".
- * <p>
+ *
  * makeTags("i", "Yay") → "<i>Yay</i>"
  * makeTags("i", "Hello") → "<i>Hello</i>"
  * makeTags("cite", "Yay") → "<cite>Yay</cite>"
